3564fe208bff345e7ffa37f7e4c01f69fb0c45c2
sakurakinomoto1 <sakuraavalon3312@gmail.com>
2025-08-27 01:17:14 -0500

    worked on home page to match figma

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import './Home.css';
-import CalendarComponent from '../components/CalendarComponent.jsx';
-import Layout from '../components/Layout';
-import banner from '../images/ieee nsm banner (1).png';
-import olivia from '../images/Olivia holding image (1).png';
-import bluediscord from '../images/discord.png'
-import bluelinkedin from '../images/linkedin.png';
-import friends from '../images/smiling friends (1).png';
+import React from "react";
+import "./Home.css";
+import CalendarComponent from "../components/CalendarComponent.jsx";
+import Layout from "../components/Layout";
+import banner from "../images/ieee nsm banner (1).png";
+import olivia from "../images/Olivia holding image (1).png";
+import bluediscord from "../images/discord.png";
+import bluelinkedin from "../images/linkedin.png";
+import friends from "../images/smiling friends (1).png";
 
 function Home() {
   return (
-    
     <Layout>
       <div className="home-container">
         {/* Hero Banner with Dark Overlay */}
@@ -20,7 +19,6 @@ function Home() {
           <div className="hero-content">
             <p>University of Houston</p>
             <h1>Institute of Electrical and Electronics Engineers</h1>
-            
           </div>
         </div>
 
@@ -29,62 +27,70 @@ function Home() {
           <div className="mission-content">
             <h3>Our Mission</h3>
             <p>
-              IEEE's core purpose is to foster technological innovation and excellence. 
-              We strive to create a space where students can find community and more 
-              accessible opportunities for research and competitions.
+              IEEE's core purpose is to foster technological innovation and <br />
+              excellence for the benefit of humanity. We strive to create a <br />
+              space where students can find community and more accessible <br />
+              opportunities for research and competitions.
             </p>
           </div>
           <div className="mission-image">
-            
             <img src={olivia} alt="IEEE Member" />
           </div>
-          
+
           <button>Become a Member</button>
         </section>
 
         {/*Events Section*/}
         <section className="events-section">
-            <CalendarComponent />
-
+          <CalendarComponent />
         </section>
 
         {/* Membership Form */}
         <section className="membership-form">
-          <div className ="form-inner">
-          <h3>Contact Us</h3>
-          <form action="https://formsubmit.co/nsm.ieeeuh@gmail.com" method="POST">
-            <div className="form-row">
-              <div className="form-group">
-                <label htmlFor="name">Name (required)</label>
-                <input type="text" name="name" id="name" required />
-                <input type="hidden" name="_subject" value="New submission!"></input>
+          <div className="form-inner">
+            <h3>Contact Us</h3>
+            <form
+              action="https://formsubmit.co/nsm.ieeeuh@gmail.com"
+              method="POST"
+            >
+              <div className="form-row">
+                <div className="form-group">
+                  <label htmlFor="name">Name (required)</label>
+                  <input type="text" name="name" id="name" required />
+                  <input
+                    type="hidden"
+                    name="_subject"
+                    value="New submission!"
+                  ></input>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email (required)</label>
+                  <input type="email" name="email" id="email" required />
+                </div>
               </div>
-              <div className="form-group">
-                <label htmlFor="email">Email (required)</label>
-                <input type="email" name="email" id="email" required />
-              </div>
-            </div>
 
-            <div className="form-row">
-              <div className="form-group">
-                <label htmlFor="role">I am...</label>
-                <select type="role" name="role" id="role">
-                  <option value="">Select...</option>
-                  <option value="student">A Student</option>
-                  <option value="professional">An Alum</option>
-                  <option value="faculty">A Sponsor</option>
-                  <option value="representative">A Company Representative</option>
-                </select>
+              <div className="form-row">
+                <div className="form-group">
+                  <label htmlFor="role">I am...</label>
+                  <select type="role" name="role" id="role">
+                    <option value="">Select...</option>
+                    <option value="student">A Student</option>
+                    <option value="professional">An Alum</option>
+                    <option value="faculty">A Sponsor</option>
+                    <option value="representative">
+                      A Company Representative
+                    </option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <label>Message</label>
+                  <textarea name="message"></textarea>
+                </div>
+                <button type="submit" className="submit-btn">
+                  Send
+                </button>
               </div>
-              <div className="form-group">
-                <label>Message</label>
-                <textarea name="message">
-
-                </textarea>
-              </div>
-              <button type="submit" className="submit-btn">Send</button>
-            </div>
-          </form>
+            </form>
           </div>
         </section>
       </div>
