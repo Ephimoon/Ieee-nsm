3893a1f08d3378b09bda1b92e58fbf8000919670
Melanie Escobar <157929578+Ephimoon@users.noreply.github.com>
2025-10-13 13:01:46 -0500

    Added partners

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -9,6 +9,11 @@ import banner from "../images/ieee nsm banner (1).png";
 import olivia from "../images/Olivia holding image.png";
 import friends from "../images/smiling friends (1).png";
 import table from "../images/working at table.jpg";
+import CSGirlsLogo from "../images/CSgirlsLOGO.png";
+import CodeCoogsLogo from "../images/CodeCoogsLOGO.png";
+import ColorStackLogo from "../images/ColorStackLOGO.png";
+import CougarettesLogo from "../images/CougarettesLOGO.png";
+import IEEEUHLogo from "../images/IEEEUHLOGO.png";
 
 const DISCORD_URL   = "https://discord.gg/2KtqWSP8t2";
 const INSTAGRAM_URL = "https://www.instagram.com/ieee_nsm";
@@ -18,6 +23,39 @@ const LINKEDIN_URL  = "https://www.linkedin.com/company/ieee-nsm/";
 import bluelinkedin from '../images/linkedin.png';*/
 
 // commenting out these imports because they cause ESLint issues, lmk if we actually needed them or not pls :heart:
+
+const PARTNERS = [
+  {
+    name: "CSgirls",
+    href: "https://www.csgirls.org/",
+    img: CSGirlsLogo,
+    alt: "CSgirls logo",
+  },
+  {
+    name: "CodeCoogs",
+    href: "https://www.codecoogs.com/",
+    img: CodeCoogsLogo,
+    alt: "CodeCoogs logo",
+  },
+  {
+    name: "ColorStack @ UH",
+    href: "https://colorstackuh.org/",
+    img: ColorStackLogo,
+    alt: "ColorStack @ UH logo",
+  },
+  {
+    name: "Cougarettes",
+    href: "https://uhcougarettes.com/",
+    img: CougarettesLogo,
+    alt: "Cougarettes logo",
+  },
+  {
+    name: "IEEE University of Houston",
+    href: "https://ieeeuh.org/",
+    img: IEEEUHLogo,
+    alt: "IEEE UH logo",
+  }
+];
 
 function Home() {
   const [contactFormResponse, setContactFormResponse] = useState("");
@@ -169,7 +207,7 @@ function Home() {
               href={LINKEDIN_URL}
               target="_blank"
               rel="noopener noreferrer">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
                 <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
               </svg>
               LinkedIn
@@ -181,6 +219,31 @@ function Home() {
         {/*Events Section*/}
         <section className="events-section">
           <CalendarComponent />
+        </section>
+
+        {/* Partners Section */}
+        <section className="partners-section" aria-labelledby="partners-heading">
+          <div className="partners-inner">
+            <h3 id="partners-heading">Partnerships</h3>
+
+            <ul className="partners-grid" role="list">
+              {PARTNERS.map((p) => (
+                <li key={p.name} className="partner-card">
+                  <a
+                    className="partner-link"
+                    href={p.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <figure className="partner-figure">
+                      <img src={p.img} alt={p.alt || p.name} loading="lazy" />
+                    </figure>
+                    <span className="partner-name">{p.name}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </section>
 
         {/* Membership Form */}
